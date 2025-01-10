41953f4ce2cec60c7efc8e9ad439889764d5a8e6
Vit_box <lizzergin11@gmail.com>
2025-01-10 18:22:32 +0300

    add nameMonth2, onkeydown = "Enter"

--- The_task_book/lg.js
+++ The_task_book/lg.js
@@ -17,6 +17,7 @@ function formatDate() {
   let day = String(date.getDate()).padStart(2, '0');
   // Получаем месяц (начиная с нуля, поэтому прибавляем 1), преобразуем его в строку и добавляем ведущий ноль, если число меньше 10
   let month = String(date.getMonth() + 1).padStart(2, '0');
+
   function nameMonth() {
     if (month === '01') {
       return 'January'
@@ -55,15 +56,29 @@ function formatDate() {
       return 'December'
     }
   }
+
   let nameMonth1 = nameMonth()
+
+
   let monthIndexToNameMap = {
     '01': 'January',
     '02': 'February',
-    // ...
+    '03': 'March',
+    '04': 'April',
+    '05': 'May',
+    '06': 'June',
+    '07': 'July',
+    '08': 'August',
+    '09': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December'
   }
-  let nameMonth2 = indexToNameMap[month];
+  let nameMonth2 = monthIndexToNameMap[month];
+
+
   // Формируем строку в формате dd name month и возвращаем её
-  return `${day} ${nameMonth1}`;
+  return `${day} ${nameMonth2}`;
 }
 
 let dateField = document.getElementById("dateField");
@@ -127,9 +142,48 @@ function renderList (arrParsed) {
   });
 }
 
+// -----------------------
+
+// Слушатели событий
+
+// 1. Слушатель в виде атрибута на html элементе
+// <button onclick="onDelete(event)">X</button>
+
+// event - объект события
+// event.target - целевой элемент на котором произошло событие
+// event.preventDefault() - отменяет стандартное браузерное поведение для какого-то события
+
+// html
+{/* <a href="www.yandex.ru" onclick="handleClick(event)"></a> */}
+
+// js
+// function handleClick(event) {
+//   event.preventDefault();
+// }
+
+// 2. Слушатель события с помощью функции addEventListener
+
+// html
+{/* <input id="input" type="text" /> */}
+
+// js
+// const input = document.querySelector('#input');
+// input.addEventListener('keydown', function (event) {
+//   console.log(this.value); // this.value === input.value (значение, введенное в input)
+
+//   if (event.key === 'Enter') {
+//     console.log('Была нажата клавиша Enter!')
+//   }
+// });
+
+
+// -----------------------
+
 renderList(arrParsed);
 
 function completed(event, id) {
+  console.log(event);
+
   let index = arrParsed.findIndex(item => item.id === id);
   arrParsed[index].completed = !arrParsed[index].completed;
   localStorage.setItem('todoList', JSON.stringify(arrParsed));// Запись значения
@@ -164,6 +218,16 @@ function addNewTask() {
     localStorage.setItem('todoList', JSON.stringify(arrParsed));// Запись значения
     renderList(arrParsed); // Отображаем новую задачу в списке
     input.value = ''; // Очищаем поле ввода
+  }
+}
+
+// 1. Добавить слушатель события на нажатие кнопки на клавиатуре
+// 2. Определить, что была нажата именно клавиша Enter
+// 3. Если нажали именно Enter, то вызываем addNewTask
+function addNewEnterTask(event) {
+  console.log(event);
+  if (event.key === 'Enter') {
+    addNewTask();
   }
 }
 
