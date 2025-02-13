94e5797c2a09d906c82e9ecf6485b48a0bb57068
Vit_box <lizzergin11@gmail.com>
2025-02-13 16:01:43 +0300

    add table body

--- The_task_book/lg2.js
+++ The_task_book/lg2.js
@@ -1,9 +1,8 @@
 //Рендер таблицы
 
-function renderList () {
+function renderTableHeader() {
     const tableHeader = document.querySelector("thead > tr");
     tableHeader.innerHTML = '';
-
 
     for (let i = 0; i < 7; i++) {
         const date = new Date();
@@ -26,8 +25,42 @@ function renderList () {
         "afterbegin",
         `<th></th>`
     )
+};
+
+renderTableHeader();
+
+function renderTableBody() {
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = '';
+
+    arrParsed.forEach(item => {
+        tableBody.insertAdjacentHTML(
+            "beforeend",
+            `<tr>
+                <td>${item.title}</td>
+                <td>${isCompleted(item.datesCompleted, 6)}</td>
+                <td>${isCompleted(item.datesCompleted, 5)}</td>
+                <td>${isCompleted(item.datesCompleted, 4)}</td>
+                <td>${isCompleted(item.datesCompleted, 3)}</td>
+                <td>${isCompleted(item.datesCompleted, 2)}</td>
+                <td>${isCompleted(item.datesCompleted, 1)}</td>
+                <td>${isCompleted(item.datesCompleted, 0)}</td>
+            </tr>`
+        )
+    })
+};
+
+renderTableBody();
+
+/**
+ * @param datesArray - ['2025-02-12', '2025-02-01']
+ * @param offset - 0
+ * @returns V / X
+ */
+function isCompleted(datesArray, offset) {
+    const date = new Date();
+    date.setDate(date.getDate() - offset); // установка даты (сегодняшняя минус offset)
+    const searchDate = date.toISOString().slice(0, 10); // получили '2025-02-12'
+
+    return datesArray.includes(searchDate) ? '✅' : '❌';
 }
-
-
-
-renderList ()
